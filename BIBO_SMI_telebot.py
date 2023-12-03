944dc4726518902dde2fdcce25cf449322c219c6
Rayyan <85599573+rayyanwong@users.noreply.github.com>
2023-12-03 16:48:18 +0800

    Update BIBO_SMI_telebot.py
    Debug

--- BIBO_SMI_telebot.py
+++ BIBO_SMI_telebot.py
@@ -8,7 +8,7 @@ from datetime import datetime,timedelta,date
 import pandas as pd
 import csv
 
-TOKEN : Final = "6178172385:AAEEbHfVWUObNOYge7gdpd413Mqxn-hhRQU"
+TOKEN : Final = ""
 BOT_USERNAME: Final = "@rayyantest3bot"
 DATE_REGEX: Final = r"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$"
 FD_NO, NAME, MASKED_IC,REG_COMPLETE, LOCATION, INFOTOEDIT, EDITNAME, STATUSBO, NOTBOCHECK, NOTBOREASON, NOTBOREASON2, NOTBOREASON3,GETSTATUSSTART, EDITSTATUS, CHECKVALIDSTATUS, UPDATEOOC2, UPDATEOOC3, UPDATEOOC4, TEMPSTATUS2, TEMPSTATUS3, TEMPSTATUS4 = range(21)
